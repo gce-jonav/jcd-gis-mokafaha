1ebc526beb25a0ab38d6672c3ae294383ae4865f
Osama Almaani <osama.maani@gmail.com>
2023-03-03 23:12:03 +0300

    add jcd logo

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,14 +4,18 @@ import { Link, Outlet } from 'react-router-dom';
 function App() {
   return (
     <div className="flex flex-col h-screen" dir="rtl">
-      <div className="flex w-full justify-between p-4">
-        <div className="flex gap-x-4">
-          <Link to="/">الخارطة الالكترونية</Link>
-          {/* <Link to="/employees">الموظفين</Link> */}
+      <div className="flex w-full justify-between items-center p-4 shadow-md" style={{ zIndex: 999 }}>
+        <div className="flex gap-x-6 items-center">
+          <img src="https://services.customs.gov.jo/JCcits/assets/img/cust_side_tr.png" alt="logo" width="140" />
+          <div className="flex gap-x-4">
+            <Link to="/" className="bg-slate-700 text-white py-2 px-4 rounded-md hover:bg-slate-500">
+              الخارطة الالكترونية
+            </Link>
+            {/* <Link to="/employees">الموظفين</Link> */}
+          </div>
         </div>
-        <h4>الجمارك الاردنية - مديرية مكافحة التهريب</h4>
         <div className="relative">
-          <IconUserCircle stroke={1.5} />
+          <IconUserCircle stroke={1.5} size={28} />
         </div>
       </div>
       <Outlet />
